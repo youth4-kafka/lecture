f7dd05d00dae0993f7cf3ea2069052e86d75ef6d
jeunghoonsong <thdwmdgns@naver.com>
2023-08-11 23:28:41 +0900

    Lesson Multi Module 설정

---
+++ app-lesson/src/main/java/lecture/AppLessonApplication.java
@@ -0,0 +1,12 @@
+package lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppLessonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppLessonApplication.class, args);
+    }
+}

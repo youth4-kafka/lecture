146a64c543c64e67c8c3d5e30264881df9609b5e
jeunghoonsong <thdwmdgns@naver.com>
2023-08-11 23:29:34 +0900

    Payment Multi Module 설정

---
+++ app-payment/src/main/java/lecture/AppPaymentApplication.java
@@ -0,0 +1,12 @@
+package lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppPaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppPaymentApplication.class, args);
+    }
+}

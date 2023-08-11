a291f869a09a43a977bbb589bda75c403bc90bb6
jeunghoonsong <thdwmdgns@naver.com>
2023-08-11 23:28:31 +0900

    Instructor Multi Module 설정

---
+++ app-instructor/src/main/java/lecture/AppInstructorApplication.java
@@ -0,0 +1,12 @@
+package lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppInstructorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppInstructorApplication.class, args);
+    }
+}

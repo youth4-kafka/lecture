37b15b1ae79a7004b2b4a941bfc251512323bd46
jeunghoonsong <thdwmdgns@naver.com>
2023-08-11 23:29:12 +0900

    Registration Multi Module 설정

---
+++ app-registration/src/main/java/lecture/AppRegistrationApplication.java
@@ -0,0 +1,12 @@
+package lecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppRegistrationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppRegistrationApplication.class, args);
+    }
+}
